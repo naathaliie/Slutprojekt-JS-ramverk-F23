77b62a355fa2a302a0b74a7e197bb9930957ce1f
naathaliie <nathalielarsson@live.se>
2024-05-02 13:43:58 +0200

    searchContext

--- src/context/SearchContext/SearchContext.tsx
+++ src/context/SearchContext/SearchContext.tsx
@@ -1,3 +1,10 @@
-/* import { createContext } from "react";
+import { createContext } from "react";
+import { GlobalSearchState, initialSearchState } from "../../types/types";
 
-const SearchContext = createContext(); */
+const SearchContext = createContext<{
+  state: GlobalSearchState; // den initiala typen
+  dispatch: React.Dispatch<Action>;
+}>({
+  state: initialSearchState, // Det initiala värdet
+  dispatch: () => null, // Standardfunktion som inte gör något
+});
