af854cc1fa009e161cea3c20972d4a71a41aa2fc
naathaliie <nathalielarsson@live.se>
2024-05-16 08:34:03 +0200

    toglleicon

--- src/components/ToggleIcon/ToggleIcon.tsx
+++ src/components/ToggleIcon/ToggleIcon.tsx
@@ -5,30 +5,49 @@ import { useDispatch } from "react-redux";
 type ToggleIconProps<T> = {
   trueIcon: string | React.ReactNode;
   falseIcon: string | React.ReactNode;
-  name: string;
-  thisDispatch: (payload: PayloadAction<T>) => PayloadAction<T>; // Ändra signatur för thisDispatch
-  thisPayload: PayloadAction<T>;
+  trueName: string;
+  falseName: string;
+  trueDispatch: (payload: PayloadAction<T>) => PayloadAction<T>;
+  truePayload: PayloadAction<T>;
+  falseDispatch: (payload: PayloadAction<T>) => PayloadAction<T>;
+  falsePayload: PayloadAction<T>;
 };
 
 const ToggleIcon = <T extends any>({
   trueIcon,
   falseIcon,
-  name,
-  thisDispatch,
-  thisPayload,
+  trueName,
+  falseName,
+  trueDispatch,
+  truePayload,
+  falseDispatch,
+  falsePayload,
 }: ToggleIconProps<T>) => {
   const [isFavorit, toggleIsFavorit] = useToggle(false);
   const dispatch = useDispatch();
 
-  const handleOnClick = () => {
+  const trueHandleOnClick = () => {
     toggleIsFavorit();
-    dispatch(thisDispatch(thisPayload)); // Anropa dispatch med thisDispatch
+    dispatch(trueDispatch(truePayload));
+  };
+
+  const falseHandleOnClick = () => {
+    toggleIsFavorit();
+    dispatch(falseDispatch(falsePayload));
   };
 
   return (
-    <span className={`${name}`} onClick={handleOnClick}>
-      {isFavorit ? trueIcon : falseIcon}{" "}
-    </span>
+    <>
+      {isFavorit ? (
+        <span className={trueName} onClick={trueHandleOnClick}>
+          {trueIcon}
+        </span>
+      ) : (
+        <span className={falseName} onClick={falseHandleOnClick}>
+          {falseIcon}
+        </span>
+      )}
+    </>
   );
 };
 
