af5c08ecd30a1ca5f99ec2bc4f49475b8fd1b3fd
naathaliie <nathalielarsson@live.se>
2024-05-19 12:17:36 +0200

    mypagenavigation

---
+++ src/components/MyPageNavigation/MyPageNavigation.tsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Menu, MenuItem } from "@mui/material";
+import { Link } from "react-router-dom";
+import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import { useState } from "react";
+
+const MyPageNavigation = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  return (
+    <div className="nav-myPage">
+      <span onClick={handleClick}>
+        <AccountCircleIcon sx={{ fontSize: "50px" }} />
+      </span>
+      <Menu
+        id="basic-menu"
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        MenuListProps={{
+          "aria-labelledby": "basic-button",
+        }}
+      >
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "hotpink", textDecoration: "none" }}
+            key={"favorits"}
+            to={"/myPage/myFavorits"}
+          >
+            Mina favoriter
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "hotpink", textDecoration: "none" }}
+            key={"readBooks"}
+            to={"/myPage/myReadBooks"}
+          >
+            Mina lästa böcker
+          </Link>
+        </MenuItem>
+        <MenuItem onClick={handleClose}>
+          <Link
+            style={{ color: "hotpink", textDecoration: "none" }}
+            key={"other"}
+            to={"/myPage/other"}
+          >
+            Övrigt
+          </Link>
+        </MenuItem>
+      </Menu>
+    </div>
+  );
+};
+
+export default MyPageNavigation;
--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -1,19 +1,9 @@
-import { Link, NavLink, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 import "./Navbar.scss";
 import SearchBar from "../SearchBar/SearchBar";
-import { Menu, MenuItem } from "@mui/material";
-import { useState } from "react";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
+import MyPageNavigation from "../MyPageNavigation/MyPageNavigation";
 
 const Navbar = () => {
-  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
-  const open = Boolean(anchorEl);
-  const handleClick = (event: React.MouseEvent<HTMLButtonElement>) => {
-    setAnchorEl(event.currentTarget);
-  };
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
   return (
     <>
       <div className="NavBar">
@@ -27,48 +17,7 @@ const Navbar = () => {
           </NavLink>
         </div>
 
-        <div className="nav-myPage">
-          <span onClick={handleClick}>
-            <AccountCircleIcon sx={{ fontSize: "50px" }} />
-          </span>
-          <Menu
-            id="basic-menu"
-            anchorEl={anchorEl}
-            open={open}
-            onClose={handleClose}
-            MenuListProps={{
-              "aria-labelledby": "basic-button",
-            }}
-          >
-            <MenuItem onClick={handleClose}>
-              <Link
-                style={{ color: "hotpink", textDecoration: "none" }}
-                key={"favorits"}
-                to={"/myPage/myFavorits"}
-              >
-                Mina favoriter
-              </Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Link
-                style={{ color: "hotpink", textDecoration: "none" }}
-                key={"readBooks"}
-                to={"/myPage/myReadBooks"}
-              >
-                Mina lästa böcker
-              </Link>
-            </MenuItem>
-            <MenuItem onClick={handleClose}>
-              <Link
-                style={{ color: "hotpink", textDecoration: "none" }}
-                key={"other"}
-                to={"/myPage/other"}
-              >
-                Övrigt
-              </Link>
-            </MenuItem>
-          </Menu>
-        </div>
+        <MyPageNavigation />
       </div>
       <Outlet />
     </>
